5e6302db9a938f89efde63ecaa7db2a1b9670dd4
vinceevogue <ferreiravincent115@gmail.com>
2025-03-21 18:03:44 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Navbar } from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Navbar />
+     <div className="container-fluid border border-success">
+        <h1 className="my-4 text-center">Quizz Test</h1>
+        <div className="row text-center">
+              <div className="col">test</div>
+              <div className="col">test</div>
+              <div className="col">test</div>
+              <div className="col">test</div>
+              <div>
+              <button className="btn btn-light btn-sm">button</button>
+              </div>
+        </div>
+     </div>
     </>
   )
 }

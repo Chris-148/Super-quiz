8003a4ade67c04529d8e45e5d46876301bd036c7
vinceevogue <ferreiravincent115@gmail.com>
2025-03-22 18:13:50 +0100

    question contexte

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { QuestionsProvider } from './context/QuestionsContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  <QuestionsProvider>
     <Router>
       <App />
     </Router>
+  </QuestionsProvider>
   </StrictMode>,
 )

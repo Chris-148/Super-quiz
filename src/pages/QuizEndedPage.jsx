94adddc42c9aa351e80878c2444b38a4e197f455
Christian Wirsching <christianwirsching@gmail.com>
2025-03-25 17:40:23 +0100

    Mapping of high scores in EndPage

--- src/pages/QuizEndedPage.jsx
+++ src/pages/QuizEndedPage.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
-import { useContext } from 'react'
+import { useContext, useEffect, useState} from 'react'
+import { QuizContext } from "../context/QuizContext"
 
 export const QuizEndedPage = () => {
+    const {currentQuiz} = useContext(QuizContext)
     
     
   return (
     <div>
-    <div>QuizEndedPage</div>
+    <div>You made it!</div>
     {/* if  */}
     <div>Your score</div>
+    {console.log(currentQuiz)}
+    {currentQuiz.score.map((scoreItem, index)=>{
+      const userId = Object.keys(scoreItem)[0]; 
+      const score = scoreItem[userId];
+      return (
+        <div key={index} className="d-flex flex-row p-2 justify-content-between align-items-center">
+        <div className="col-6">{userId}</div>
+        <div className="col-6">{score}</div>
+      </div>
+      )
+    })}
     {/* map of the Highscores */}
     </div>
   )
